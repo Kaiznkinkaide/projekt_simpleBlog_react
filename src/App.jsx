91ae2226c13ba83a8adae3c255ba7a18b867149e
Kaiznkinkaide <daniel.th.cohen@gmail.com>
2024-01-22 14:45:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<Routes>
+  <Route/>
+</Routes>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+    return ( 
+        <>
+        <nav>
+        </nav>
+        </>
+     );
+}
+ 
+export default Nav;
